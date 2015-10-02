1e054fdc1a98e5b614567535945fef23489d7c45
Hayden <haydentwestbrook@gmail.com>
2015-10-02 14:35:44 -0700

    Reremoved excess space and comments

--- Calculator/app/src/main/java/com/example/j/myapplication/Calculator.java
+++ Calculator/app/src/main/java/com/example/j/myapplication/Calculator.java
@@ -17,69 +17,54 @@ public class Calculator extends Activity {
         setContentView(R.layout.activity_hello_world);
 
         showResult = (EditText)findViewById(R.id.result_id);
-
-
     }
     public void btn1Clicked(View v){
         insert(1);
-
     }
 
     public void btn2Clicked(View v){
         insert(2);
-
     }
     public void btn3Clicked(View v){
         insert(3);
-
     }
     public void btn4Clicked(View v){
         insert(4);
-
     }
     public void btn5Clicked(View v){
         insert(5);
-
     }
     public void btn6Clicked(View v){
         insert(6);
     }
     public void btn7Clicked(View v){
         insert(7);
-
     }
     public void btn8Clicked(View v){
         insert(8);
-
     }
     public void btn9Clicked(View v){
         insert(9);
-
     }
     public void btnPlusClicked(View v){
         perform();
         op = '+';
-
     }
 
     public void btnMinClicked(View v){
         perform();
         op = '-';
-
     }
     public void btnDivClicked(View v){
         perform();
         op = '/';
-
     }
     public void btnMultClicked(View v){
         perform();
         op = '*';
-
     }
     public void btnEqualClicked(View v){
         calculate();
-
     }
 
     public void btnCEClicked(View v){
@@ -90,7 +75,6 @@ public class Calculator extends Activity {
         insert(0);
     }
     private void reset() {
-        // TODO Auto-generated method stub
         str ="";
         op ='q';
         num = 0;
@@ -98,7 +82,6 @@ public class Calculator extends Activity {
         showResult.setText("");
     }
     private void insert(int j) {
-        // TODO Auto-generated method stub
         str = str+Integer.toString(j);
         num = Integer.valueOf(str).intValue();
         showResult.setText(str);
@@ -106,12 +89,10 @@ public class Calculator extends Activity {
 
     }
     private void perform() {
-        // TODO Auto-generated method stub
         str = "";
         numtemp = num;
     }
     private void calculate() {
-        // TODO Auto-generated method stub
         if(op == '+')
             num = numtemp+num;
         else if(op == '-')
